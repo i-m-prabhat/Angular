1d34758b04c2791f577339e289864770ec0a5efb
Prabhat <117756490+i-m-prabhat@users.noreply.github.com>
2024-02-18 22:07:06 +0530

    styles

--- ekart/src/app/header/header.component.ts
+++ ekart/src/app/header/header.component.ts
@@ -2,7 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: "app-header",
-    template: `<h3>ekart</h3>`
+    // template: `
+    //     <div class="container">
+    //         <h1>ekart</h1>
+    //     </div>        
+    // `,
+    templateUrl: "header.component.html",
+    // styles: [
+    //     'h1{font-size:32px;background-color:red;}'
+    // ],
+    styleUrls: ['header.component.css']
 })
 export class HeaderComponent
 {
